6150eae62599cbe1034e7709abb31b87f3361095
Kenny Pflug <kenny.pflug@live.de>
2017-01-30 13:26:04 +0100

    added example for processor optimizations

---
+++ MultithreadingWithoutSynchronization/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace MultithreadingWithoutSynchronization
+{
+    internal class Program
+    {
+        private static bool _isComplete;
+
+        private static void Main()
+        {
+            _isComplete = false;
+            var thread = new Thread(Spin);
+            thread.Start();
+
+            Thread.Sleep(2500);
+
+            _isComplete = true;
+            thread.Join();
+            Console.WriteLine("Other thread completed spinning");
+        }
+
+        private static void Spin()
+        {
+            var isActive = false;
+            while (_isComplete == false)
+                isActive = !isActive;
+        }
+    }
+}
