4beac6aa618379e1a271c79238cc7038b810a1b6
Kenny Pflug <kenny.pflug@live.de>
2017-01-30 15:01:15 +0100

    added LockFreeCopying example

---
+++ MultithreadedCopying/LockFreeMultiThreadedCopying.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Light.GuardClauses;
+
+namespace MultithreadedCopying
+{
+    public sealed class LockFreeMultiThreadedCopying
+    {
+        private readonly int[] _newArray;
+        private readonly int[] _oldArray;
+        private int _currentIndex = -1;
+
+        public LockFreeMultiThreadedCopying(int[] oldArray, int[] newArray)
+        {
+            newArray.Length.MustBeGreaterThan(oldArray.Length);
+
+            _oldArray = oldArray;
+            _newArray = newArray;
+        }
+
+        public void Run()
+        {
+            var tasks = Enumerable.Range(0, Environment.ProcessorCount)
+                                  .Select(i => Task.Run(new Action(CopyFromOldToNew)))
+                                  .ToArray();
+            Task.WaitAll(tasks);
+        }
+
+        private void CopyFromOldToNew()
+        {
+            while (true)
+            {
+                var i = Interlocked.Increment(ref _currentIndex);
+                if (i >= _oldArray.Length)
+                    return;
+
+                _newArray[i] = _oldArray[i];
+            }
+        }
+    }
+}
---
+++ MultithreadedCopying/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MultithreadedCopying
+{
+    internal class Program
+    {
+        private const int SmallArraySize = 100000000; // 10 Million strings
+        private const int LargeArraySize = 150000000; // 15 Million strings
+
+        private static void Main()
+        {
+            var largeArray = InitializeLargeArray();
+            var evenLargerArray = new int[LargeArraySize];
+
+            var stopwatch = Stopwatch.StartNew();
+            //CopySingleThreaded(largeArray, evenLargerArray);
+            new LockFreeMultiThreadedCopying(largeArray, evenLargerArray).Run();
+
+            Console.WriteLine($"Copying {largeArray.Length:N0} items took {stopwatch.Elapsed.TotalMilliseconds:N}ms");
+        }
+
+        private static int[] InitializeLargeArray()
+        {
+            var array = new int[SmallArraySize];
+            var random = new Random(19943832);
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+            }
+
+            return array;
+        }
+
+        private static void CopySingleThreaded(int[] oldArray, int[] newArray)
+        {
+            for (var i = 0; i < oldArray.Length; i++)
+            {
+                newArray[i] = oldArray[i];
+            }
+        }
+    }
+}
